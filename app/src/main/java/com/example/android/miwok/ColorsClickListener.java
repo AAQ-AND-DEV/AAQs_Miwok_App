a7f0392878dc97ced2903055cbc6ddfe34c481eb
AquaticNewKeyHack <aaronquaday@gmail.com>
2019-01-30 13:09:44 -0800

    seems i needed to make some changes when calling listener from external java file.  DONE here via the view.getContext() route

--- app/src/main/java/com/example/android/miwok/ColorsClickListener.java
+++ app/src/main/java/com/example/android/miwok/ColorsClickListener.java
@@ -1,13 +1,14 @@
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
 
 public class ColorsClickListener implements View.OnClickListener {
     @Override
     public void onClick(View v) {
-        Toast.makeText(v.getContext(),
-            "open colors list", Toast.LENGTH_SHORT)
-                .show();
+        Intent colorsIntent = new Intent(v.getContext(), ColorsActivity.class);
+
+        v.getContext().startActivity(colorsIntent);
     }
 }
