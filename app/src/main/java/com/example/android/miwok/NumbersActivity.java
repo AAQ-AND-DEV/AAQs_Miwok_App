a5c848e566598fc4e14f2eabfa26cedd339f8578
AquaticNewKeyHack <aaronquaday@gmail.com>
2019-01-30 13:41:14 -0800

    added array of Strings for words representing numbers 1-10 in english added test log to print the array of numbers to log.i

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        String[] words = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        //gives the ref to the storage location of the array
+        Log.i("NumbersActivity.class", words.toString());
+        for (String word: words
+             ) {
+            Log.i("numbersAct.class", word);
+        }
+
     }
 }
