838fda25e65b684d9c97ba66535c24f50150adb5
AquaticNewKeyHack <aaronquaday@gmail.com>
2019-01-31 12:44:38 -0800

    added a for loop for adding colors (as String) -- saw we'll be making Word objects to contain the info that includes a translation in the future. there's that to look forward to!!

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,6 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("red");
+        words.add("mustard yellow");
+        words.add("dusty yellow");
+        words.add("green");
+        words.add("brown");
+        words.add("gray");
+        words.add("black");
+        words.add("white");
+
+        LinearLayout colorsLO = findViewById(R.id.colors_root_LO);
+        for (int i = 0; i<words.size(); i++){
+            TextView tv = new TextView(this);
+            tv.setText(words.get(i));
+            colorsLO.addView(tv, i);
+        }
     }
 }
