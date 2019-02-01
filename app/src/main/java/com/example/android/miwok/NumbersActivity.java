9a0d6e463ec055dc0859b8bc7907d865c12f79af
AquaticNewKeyHack <aaronquaday@gmail.com>
2019-01-31 16:18:26 -0800

    used gridView -- surprised to learn gravity center doesn't center the text within each grid element

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -25,7 +26,7 @@ public class NumbersActivity extends AppCompatActivity {
         //gives the ref to the storage location of the array
         Log.i("NumbersActivity.class", words.toString());
 
-        ListView listView = findViewById(R.id.numbers_list);
+        GridView listView = findViewById(R.id.numbers_list);
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
         listView.setAdapter(itemsAdapter);
 
